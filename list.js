b7034badd5571859788c6a2df840b507eab2caf8
Kim Mi Reu <samkmr0220@gmail.com>
2022-07-25 16:13:45 +0900

    implement-paintToDo

--- list.js
+++ list.js
@@ -2,21 +2,17 @@ const listForm = document.querySelector('.listForm');
 const inputBox = document.getElementById('inputField');  // 할 일 입력창
 const toDoList = document.querySelector('.toDoList');    // 할 일 리스트창
 
-listForm.addEventListener('submit', function(e) {
-  e.preventDefault();    
+function paintToDo(newToDo) {
   const list = document.createElement('li');     
   const span = document.createElement('span');
   const button = document.createElement('button');
 
-  if (inputBox.value) {
-    list.appendChild(span);
-    list.appendChild(button);  
-    span.innerText = inputBox.value;
-    button.innerText = 'X';    
-    toDoList.appendChild(list);  
-    inputBox.value= "";   
-  }  
-     
+  list.appendChild(span);
+  list.appendChild(button);  
+  toDoList.appendChild(list);  
+  span.innerText = newToDo;
+  button.innerText = '❌';    
+
   list.addEventListener('click', function() {
     if (list.style.textDecoration == 'none') {
       list.style.textDecoration = 'line-through';
@@ -31,4 +27,15 @@ listForm.addEventListener('submit', function(e) {
     const removingOne = e.target.parentElement;
     removingOne.remove();
   })
-})
+}
+
+function handleToDoSubmit(event) {
+  event.preventDefault();    
+  if (inputBox.value) {
+    const newToDo = inputBox.value;
+    paintToDo(newToDo);
+    inputBox.value= "";  
+  }
+} 
+
+ listForm.addEventListener('submit', handleToDoSubmit);
