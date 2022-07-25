4f09eb1802e1964d419d70f7b0a417e958d9c13e
Kim Mi Reu <samkmr0220@gmail.com>
2022-07-25 16:40:32 +0900

    localStorage

--- list.js
+++ list.js
@@ -2,15 +2,23 @@ const listForm = document.querySelector('.listForm');
 const inputBox = document.getElementById('inputField');  // 할 일 입력창
 const toDoList = document.querySelector('.toDoList');    // 할 일 리스트창
 
+let toDos = [];
+const TODOS_KEY = "todos";
+
+function saveToDos() {
+  localStorage.setItem(TODOS_KEY, JSON.stringify(toDos));
+}
+
 function paintToDo(newToDo) {
-  const list = document.createElement('li');     
+  const list = document.createElement('li');    
+  list.id = newToDo.id; 
   const span = document.createElement('span');
   const button = document.createElement('button');
 
   list.appendChild(span);
   list.appendChild(button);  
   toDoList.appendChild(list);  
-  span.innerText = newToDo;
+  span.innerText = newToDo.text;
   button.innerText = '❌';    
 
   list.addEventListener('click', function() {
@@ -22,10 +30,11 @@ function paintToDo(newToDo) {
     }
   })
   
-  button.addEventListener('click', function(e) {
-    e.preventDefault();  
-    const removingOne = e.target.parentElement;
+  button.addEventListener('click', function(event) {
+    const removingOne = event.target.parentElement;
     removingOne.remove();
+    toDos = toDos.filter((toDo) => toDo.id !== parseInt(removingOne.id));
+    saveToDos();
   })
 }
 
@@ -33,9 +42,23 @@ function handleToDoSubmit(event) {
   event.preventDefault();    
   if (inputBox.value) {
     const newToDo = inputBox.value;
-    paintToDo(newToDo);
     inputBox.value= "";  
+    const newToDoObject = {
+      text: newToDo,
+      id: Date.now(),
+    };
+    toDos.push(newToDoObject);
+    paintToDo(newToDoObject);
+    saveToDos();
   }
 } 
 
  listForm.addEventListener('submit', handleToDoSubmit);
+
+ const savedToDos = localStorage.getItem(TODOS_KEY);
+
+ if (savedToDos !== null) {
+  const parsedToDos = JSON.parse(savedToDos);
+  toDos = parsedToDos;
+  parsedToDos.forEach(paintToDo)
+ }
